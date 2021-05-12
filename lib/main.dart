d62cf665df51335bfcfaefb3a9b6e46d05430f56
Greg Hughes <dcgg_101@hotmail.com>
2021-05-11 20:41:11 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop/screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato'),
+      home: ProductsOverViewScreen(),
+    );
+  }
+}
