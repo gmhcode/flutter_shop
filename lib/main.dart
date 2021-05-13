3c64c696d8f08b346cea4ea021e9387b4fabc2b4
Greg Hughes <dcgg_101@hotmail.com>
2021-05-12 20:14:46 -0600

    integrated provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/providers/products.dart';
 import 'package:flutter_shop/screens/product_detail.dart';
 import 'package:flutter_shop/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato'),
-      home: ProductsOverViewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
+    //only Listeners will update with changeNotifierProvider
+    return ChangeNotifierProvider(
+      //Products is basically an observable object
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
+        home: ProductsOverViewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_shop/models/product.dart';
+import 'package:flutter_shop/widgets/product_item.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
+
+class ProductsGrid extends StatelessWidget {
+  // const ProductsGrid({
+  //   Key key,
+  //   @required this.loadedProducts,
+  // }) : super(key: key);
+
+  // final List<Product> loadedProducts;
+
+  @override
+  Widget build(BuildContext context) {
+    //Goes from parent to parent looking for products provider. which is in main
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ProductItem(
+        products[i].id,
+        products[i].title,
+        products[i].imageUrl,
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
