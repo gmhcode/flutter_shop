1c7b68491cf360d9bac52f5c31be223d076d1223
Greg Hughes <dcgg_101@hotmail.com>
2021-05-12 21:04:37 -0600

    added listener to product_detail

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/providers/products.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -9,11 +11,17 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String;
+    final productId = ModalRoute.of(context).settings.arguments
+        as String; //This gets passed the Id
+    //setting listen to false means the page wont update if it is currently being shown
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
